fff299ef42319cd3a07586c78b7f0f4f855f5bb1
JuHyuk Lim <nyclo0705@gmail.com>
2025-01-20 16:07:23 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/org/fontory/fontorybe/FonToryBeApplication.java
@@ -0,0 +1,13 @@
+package org.fontory.fontorybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonToryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FonToryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fontory/fontorybe/FonToryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fontory.fontorybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonToryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
